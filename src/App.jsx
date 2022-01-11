7898e707d1be19b6be8f0a0fe628b62bcd3eda59
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2022-01-11 13:35:32 -0400

    refactor: Implement hook useFetch and Add handleSearch for component child

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 import List from "./components/List";
 import PriceInput from "./components/PriceInput"; // eslint-disable-line
 import Search from "./components/Search";
+import { useFetch } from "./hooks/useFetch";
+
+const USERS_URL = "https://jsonplaceholder.typicode.com/users";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const { data, error, loading } = useFetch(USERS_URL);
+
+  if (error) {
+    console.log(error);
+  }
+
   const handleSearch = (event) => {
-    console.log(event.target.value, "cached by handleSearch");
+    setSearchTerm(event.target.value);
   };
   return (
     <div className="flex flex-col  bg-slate-400 p-6">
       <div className="p-6">
         <Search onSearch={handleSearch} />
       </div>
+      <span className="flex flex-col max-w-md my-0 mx-auto ">
+        {searchTerm && (
+          <p className="block text-2xl font-medium text-white p-2">
+            Searching for : <strong>{searchTerm}</strong>
+          </p>
+        )}
+      </span>
       <div className="py-6">
-        <List />
+        {loading && <div>Loading...</div>}
+        {data && <List users={data} searchTerm={searchTerm} />}
       </div>
     </div>
   );
